501810a067be69b87bd6c0620652d1eadf6335bf
degiwhlo2co <147834857+degiwhlo2co@users.noreply.github.com>
2026-07-04 20:09:16 -0400

    fixed a couple of grammatical errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,9 +10,9 @@ export default function About() {
       <p>
         I am simply a person looking for more meaning in my life, and through creation I tend to find it. 
         I believe purpose is something we create for ourselves, and I have lacked in creation as of late. In my 
-        younger days I did all the many of us were told to do: go to college, have a family,
+        younger days I did all the things many of us were told to do: go to college, have a family,
         work toward that American dream. While there were some joys to be had, I still felt I should
-        be doing something different. So I started over. I went back to college for computer science,
+        be doing something different. So, I started over. I went back to college for computer science,
         where I learned a new way of building things. I still have a lot to learn, but here 
         is where I start. Not just a site, not just a group of projects, but a world
         where I can share, explore, and connect with other curious minds. So if you find yourself here,
