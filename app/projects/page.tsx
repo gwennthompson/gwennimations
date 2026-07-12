038b82242c0c9a52a383b9f55f972ef0be1c6ef8
degiwhlo2co <147834857+degiwhlo2co@users.noreply.github.com>
2026-07-11 20:17:37 -0400

    added GitHub link to projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,8 @@ export default function Projects() {
     <div>
       <h1>Projects</h1>
       <p>This is my projects page.</p>
+      <p>Here is where you will find my projects as they develop: 
+      <a href="https://www.github.com/gwennthompson"> Gwenn's projects</a></p>
     </div>
   );
 }
