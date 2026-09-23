3daa02d004299188df519acade5f1c81c330b5e4
degiwhlo2co <147834857+degiwhlo2co@users.noreply.github.com>
2026-09-23 15:06:23 -0400

    background color added to Autumn Air

--- app/library/poetry/autumn-air/page.tsx
+++ app/library/poetry/autumn-air/page.tsx
@@ -4,22 +4,26 @@ export default function AutumnAir() {
             <h1>Autumn Air</h1>
 
             <p>
-                The light so long and bright<br />
-                Finally makes space for the glorious night<br />
-                Welcome back, my calming friend<br />
-                I wondered if it would ever end<br />
+                <span className="rounded-sm bg-green-900/15">
+                The light so long and bright<br/>
+                Finally makes space for the glorious night<br/>
+                Welcome back, my calming friend<br/>
+                I wondered if it would ever end<br/>
                 My champion of inner me<br />
-                A space for peace you'll always be<br />
-                The cool wind swiping across my face<br />
-                Each leaf floating to find its place<br />
-                Upon the forest floor they lie<br />
-                To feed the others as they die<br />
-                An old life given<br />
-                A new life born<br / >
+                A space for peace you'll always be<br/>
+                The cool wind swiping across my face<br/>
+                Each leaf floating to find its place<br/>
+                Upon the forest floor they lie<br/>
+                To feed the others as they die<br/>
+                </span>
+                  <span className="rounded-sm bg-amber-900/15">
+                An old life given<br/>
+                A new life born<br/ >
                 There's only joy<br/>
-                No need to mourn<br />
-                Fall is finally here at last<br />
+                No need to mourn<br/>
+                Fall is finally here at last<br/>
                 Let go of grief from seasons passed
+                 </span>
             </p>
         </article>
     );
